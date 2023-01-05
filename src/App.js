4151f47699c6e979ea52053226ff5abd7836615e
Texi Schaeffer <texirae.schaeffer@gmail.com>
2023-01-05 10:09:16 -0600

    added html and imports into app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { Route, Link } from "react-router-dom";
+
+import Home from './Components/Homepage'
+import OrderForm from './Components/Order_Form'
+import OrderConfirmation from './Components/Order_confirmation'
+
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="App">
+      <nav>
+        <h1 className="site-header">Bloomtech Eats</h1>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/">Help</Link>
+        </div>
+      </nav>
+      
+    </div>
   );
 };
 export default App;
