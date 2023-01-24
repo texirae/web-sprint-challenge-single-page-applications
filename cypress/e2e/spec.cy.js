8a9189a71f0cc17f39bd1e2be7deefd5f11a9550
Texi Schaeffer <texirae.schaeffer@gmail.com>
2023-01-24 12:56:31 -0600

    wrote test for text box inputs

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,50 @@
+describe('Pizza App', () => {
+
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/pizza")
+  })
+
+  const nameInput = () => cy.get('input[name=name]')
+  const pepperoniInput = () => cy.get('input[name=pepperoni')
+  const sausageInput = () => cy.get('input[name=sausage')
+  const baconInput = () => cy.get('input[name=bacon')
+  const chickenInput = () => cy.get('input[name=chicken')
+  const mushroomsInput = () => cy.get('input[name=mushrooms')
+  const peppersInput = () => cy.get('input[name=peppers')
+  const onionsInput = () => cy.get('input[name=onions')
+  const pineapplesInput = () => cy.get('input[name=pineapples')
+  const olivesInput = () => cy.get('input[name=olives')
+  const instructionsInput = () => cy.get('input[name=instructions')
+  const orderBtn = () => cy.get('button[id="order-button')
+
+  //test that all elements are showing up
+  it('the proper elements are showing', () => {
+    nameInput().should('exist')
+    pepperoniInput().should('exist')
+    sausageInput().should('exist')
+    baconInput().should('exist')
+    chickenInput().should('exist')
+    mushroomsInput().should('exist')
+    peppersInput().should('exist')
+    onionsInput().should('exist')
+    pineapplesInput().should('exist')
+    olivesInput().should('exist')
+    instructionsInput().should('exist')
+    orderBtn().should('exist')
+  })
+
+  //test for text box inputs
+  it('can type in the inputs', () => {
+    nameInput()
+      .should('have.value', '')
+      .type('Texi Schaeffer')
+      .should('have.value', "Texi Schaeffer")
+
+    instructionsInput()
+      .should('have.value', '')
+      .type('Burn the crust')
+      .should('have.value', "Burn the crust")
+  })
+
+  
+})
